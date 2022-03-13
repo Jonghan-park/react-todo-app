2748deecf7433bb97c6caa4863d185a090d6952a
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-12 20:15:39 -0700

    Add a function to store input

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,23 @@ import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return
+        }
+
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos)
+        console.log(...todos);
+    }
   
     return (
     <div>
         <h1>What's the plan for Today?</h1>
-        <TodoForm />
+        <TodoForm onSubmit={addTodo} />
     </div>
   )
 }
